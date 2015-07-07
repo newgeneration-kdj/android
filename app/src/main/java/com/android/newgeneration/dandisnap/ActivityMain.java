d6c0ab580058409e3661aed55499ad46f658b38b
천보경 <2003young@naver.com>
2015-07-08 00:11:52 +0900

    15.07.08 회원가입 및 로그인 화면 구현 2차

--- app/src/main/java/com/android/newgeneration/dandisnap/ActivityMain.java
+++ app/src/main/java/com/android/newgeneration/dandisnap/ActivityMain.java
@@ -15,31 +15,31 @@ import butterknife.OnClick;
 
 public class ActivityMain extends FragmentActivity {
     @InjectView(R.id.main_txt_title) TextView mTxtMenuTitle;
-    @InjectView(R.id.signup) Button signup;
-    @InjectView(R.id.login) Button login;
+    @InjectView(R.id.signup_btn) Button signup_btn;
+    @InjectView(R.id.login_btn) Button login_btn;
     @InjectView(R.id.layout1) FrameLayout layout1;
     @InjectView(R.id.layout2) FrameLayout layout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.example2);
         ButterKnife.inject(this);
     }
 
-    @OnClick({R.id.signup, R.id.login})
+    @OnClick({R.id.signup_btn, R.id.login_btn})
     void onButtonClick(Button btn){
         switch(btn.getId()){
-            case R.id.signup:
-                signup.setTextColor(Color.WHITE);
-                login.setTextColor(Color.GRAY);
+            case R.id.signup_btn:
+                signup_btn.setTextColor(Color.WHITE);
+                login_btn.setTextColor(Color.GRAY);
                 layout1.setVisibility(View.VISIBLE);
                 layout2.setVisibility(View.GONE);
 
                 break;
-            case R.id.login:
-                login.setTextColor(Color.WHITE);
-                signup.setTextColor(Color.GRAY);
+            case R.id.login_btn:
+                login_btn.setTextColor(Color.WHITE);
+                signup_btn.setTextColor(Color.GRAY);
                 layout1.setVisibility(View.GONE);
                 layout2.setVisibility(View.VISIBLE);
                 break;
--- app/src/main/java/com/android/newgeneration/dandisnap/MainActivity.java
+++ app/src/main/java/com/android/newgeneration/dandisnap/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.example);
+        setContentView(R.layout.example2);
 
     }
 }
