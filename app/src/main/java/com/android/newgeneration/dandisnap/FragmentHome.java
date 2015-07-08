745a6803191d102c88ba534b5c08e3142b35a22f
주승환 <tmdghks584@gmail.com>
2015-07-08 19:02:15 +0900

    15.07.08 메인화면 구현2

--- app/src/main/java/com/android/newgeneration/dandisnap/FragmentHome.java
+++ app/src/main/java/com/android/newgeneration/dandisnap/FragmentHome.java
@@ -6,14 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentHome extends Fragment{
+public class FragmentHome extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-
         return v;
     }
 
