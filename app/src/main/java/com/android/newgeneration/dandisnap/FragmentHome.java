e1e6342aeb7db3e3e433da603e25d8e54551f8dc
주승환 <tmdghks584@gmail.com>
2015-07-08 19:02:33 +0900

    Revert "15.07.08 메인화면 구현2"
    This reverts commit 745a6803191d102c88ba534b5c08e3142b35a22f.

--- app/src/main/java/com/android/newgeneration/dandisnap/FragmentHome.java
+++ app/src/main/java/com/android/newgeneration/dandisnap/FragmentHome.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentHome extends Fragment {
+public class FragmentHome extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+
         return v;
     }
 
